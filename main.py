e328592c9e48590c4b16ce3e1471ab85e0ae8f7e
LAKSHAY184 <57940742+LAKSHAY184@users.noreply.github.com>
2020-09-30 19:17:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import pyttsx3 #pip install pyttsx3
-import speech_recognition as sr #pip install speechRecognition
+import pyttsx3 
+import speech_recognition as sr 
 import datetime
-import wikipedia #pip install wikipedia
+import wikipedia
 import webbrowser
 import os
 import smtplib
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-# print(voices[1].id)
 engine.setProperty('voice', voices[0].id)
 
 
